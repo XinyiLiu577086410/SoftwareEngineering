0f4cd9c04cccaa80d6fcb3a3b5b7394eada50046
Ikaros1264 <acgzone1264@gmail.com>
2024-10-23 20:13:32 +0800

    new file:   src/views/User.vue

---
+++ Src/frontend/src/views/User.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <el-container>
+      <el-header>
+        <el-button>User header</el-button>
+      </el-header>
+    </el-container>
+    <el-container>
+      <el-aside>
+        <el-button>User aside</el-button>
+      </el-aside>
+    </el-container>
+  </template>
