5d40056b813a0558414278ec144506e0b4c67cc3
Ikaros1264 <acgzone1264@gmail.com>
2024-10-23 20:12:43 +0800

    Feat: first page

--- Src/frontend/src/App.vue
+++ Src/frontend/src/App.vue
@@ -1,85 +1,103 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+const isCollapse = ref(false)
+const circleUrl = 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+const input = ref('')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="full">
+    <el-container>
+      <el-aside>
+        <!-- 目录栏 -->
+        <el-menu default-active="2" class="el-menu-vertical" :collapse="isCollapse">
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>Navigator One</span>
+            </template>
+            <el-menu-item-group>
+              <template #title><span>Group One</span></template>
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title><span>item four</span></template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <template #title>Navigator Two</template>
+          </el-menu-item>
+        </el-menu>
+        <el-aside style="height: 40%;">
+        </el-aside>
+        <!-- 头像 -->
+        <el-avatar :size="50" :src="circleUrl" />
+        <!-- 登出按钮 -->
+        <el-button>登出</el-button>
+      </el-aside>
+      <el-main>
+          <el-main style="height: 80%;">
+          </el-main>
+          <!-- 输入框 -->
+          <el-input
+          v-model="input"
+          :autosize="{ minRows: 1, maxRows: 5 }"
+          type="textarea"
+          placeholder="Please input"
+          clearable
+        />
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.full {
+  height: 100%;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.el-container {
+  height: 100%;
+  width: 100%;
+  /* display: flex; */
+  /* flex-direction: column;
+  justify-content: space-between; */
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.el-main {
+  background-color: #fbf6f6;
+  color: #333;
 }
 
-nav a:first-of-type {
-  border: 0;
+.el-menu {
+  background-color: #d3dce6;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.el-menu-vertical:not(.el-menu--collapse) {
+  /* width: 200px; */
+  min-height: 400px;
 }
+
 </style>
